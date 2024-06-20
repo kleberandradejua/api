4ce9fc5cccd261a30bd8c1c5230d4a9fc7f4d081
Kleber de Andrade <kleberandrade98@gmail.com>
2024-06-20 08:50:18 -0300

    Código para buscar cep do 4Devs.

---
+++ cypress.js
@@ -0,0 +1,25 @@
+import puppeteer from 'puppeteer';
+
+const delay = (delayInms) => {
+  return new Promise(resolve => setTimeout(resolve, delayInms));
+};
+
+async function buscacep(estado, cidade) {
+
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage()
+  await page.goto('https://www.4devs.com.br/gerador_de_cep');
+  await page.select('#cep_estado', estado.toUpperCase());
+  await delay(200)
+  await page.select('#cep_cidade', cidade);
+  await page.screenshot({ path: 'teste.png' })
+  await page.click('#btn_gerar_cep')
+  await delay(200)
+
+  const cep = await page.$eval('#cep', el => el.innerText);
+  console.log(cep)
+  
+  await browser.close();  
+}
+
+buscacep('RN', '7148')
