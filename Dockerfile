78ac6120e628aa7d7a21efbbeab4934db24b2dd1
Kleber de Andrade <kleberandrade98@gmail.com>
2024-06-24 10:17:19 -0300

    Teste config de porta

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM ghcr.io/puppeteer/puppeteer:22.11.2
 
-FROM nginx
-EXPOSE 11879
-
 ENV PUPPERTEER_SKIP_CHROMIUM_DOWNLOAD=true \
     PUPPERTEER_EXECUTABLE_PATH=/usr/bin/google-chrome-stable
     
--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require('express')
 const buscaCep = require('./gera_cep.js');
-
-
+const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 11879;
+const PORT = process.env.PORT || 4000;
 
-app.get('/', async (req, res) => {
-    res.send(await buscaCep(req.query.uf, req.query.cod_cidade))
+app.get('/', (req, res) => {
+    res.send('hello world!')
 })
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
 })
+
+/*
+res.send(await buscaCep(req.query.uf, req.query.cod_cidade))*/
