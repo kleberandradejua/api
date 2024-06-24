061f1dc5457830d9697cc3878239424e3a3321fe
Kleber de Andrade <kleberandrade98@gmail.com>
2024-06-24 14:06:10 -0300

    Teste de porta 3

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const buscaCep = require('./gera_cep.js');
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 10000;
 
 app.get('/', (req, res) => {
     res.send('hello world!')
