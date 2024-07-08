0a994b7cb7a14a412992df155eafbe82026064b5
Kleber de Andrade <kleberandrade98@gmail.com>
2024-07-08 09:47:13 -0300

    Teste render 4

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-    res.send({ message: 'Hello World' });
-});
-/*
 app.get('/', async (req, res) => {
     res.send(await buscaCep(req.query.uf, req.query.cod_cidade))
-})*/
+})
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
